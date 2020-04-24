a3bcb8ff8273f5d0e7b6f94a450373a8d6d5778e
Vít Koma <vit.koma@memsource.com>
2020-04-24 17:46:48 +0200

    Use nio in pipeFileToDriver

--- src/test/java/koma/homework/process/TopologyBuilderTest.java
+++ src/test/java/koma/homework/process/TopologyBuilderTest.java
@@ -17,9 +17,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class TopologyBuilderTest {
 
@@ -88,16 +91,12 @@ public class TopologyBuilderTest {
     }
 
     private void pipeFileToDriver() {
-        try (InputStream is = TopologyBuilderTest.class.getResourceAsStream("/kafka-messages.jsonline")) {
-            if (is != null) {
-                try (InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
-                     BufferedReader reader = new BufferedReader(isr)) {
-                    reader.lines()
-                        .filter(line -> !line.isBlank())
-                        .forEach(line -> testDriver.pipeInput(factory.create((Integer) null, line)));
-                }
-            }
-        } catch (IOException e) {
+        try {
+            Stream<String> stream = Files.lines(Paths.get(getClass().getResource("/kafka-messages.jsonline").toURI()));
+            stream
+                .filter(line -> !line.isBlank())
+                .forEach(line -> testDriver.pipeInput(factory.create((Integer) null, line)));
+        } catch (Exception e) {
             throw new RuntimeException("Error while reading kafka-messages.jsonline", e);
         }
     }
